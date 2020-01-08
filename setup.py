2616602e255bc1b1907f5dd0acca38c044727809
Emrah Cimren <ecimren@starbucks.com>
2020-01-08 02:01:17 -0800

    Updated distribution

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="cimren-helpers",
-    version="1.0.0",
+    version="1.0.1",
     description="Helper functions",
     long_description=README,
     long_description_content_type="text/markdown",
